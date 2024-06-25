637b8ba6bef7d1a69b0dd36243bdfb536a0e14a5
thiagolboliveira1 <thiagolboliveira@hotmail.com>
2024-06-25 16:19:53 -0300

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,7 +1,7 @@
 
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/thiagolboliveira1/js-developer-portfolio/main/data/profile.json'
+    const url = 'https://raw.githubusercontent.com/thiagolboliveira1/Portfolio/main/data/profile.json'
    const fetching = await fetch(url)
    return await fetching.json()
 }
