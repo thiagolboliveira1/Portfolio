acd8664c273296fc4bb32d18e4420a3b476a20a4
thiagolboliveira1 <thiagolboliveira@hotmail.com>
2024-06-26 16:51:18 -0300

    Atualizando json

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,13 +36,42 @@ function updateProfileInfo(profileData) {
     }
 
     const hardSkills = document.getElementById('hardSkills');  // Certifique-se de que este ID exista no HTML
-if (hardSkills) {
-    hardSkills.innerHTML = profileData.skills.hardSkills.map(skill => `<li><img src="${skill.logo}" alt="${skill.name}" title="${skill.name}"></li>`).join('');
-}
+    if (hardSkills) {
+        hardSkills.innerHTML = profileData.skills.hardSkills.map(skill => `<li><img src="${skill.logo}" alt="${skill.name}" title="${skill.name}"></li>`).join('');
+    }
 
-}
+    const languages = document.getElementById('languages')  // Certifique-se de que este ID exista no HTML
+    if (languages) {
+        languages.innerHTML = profileData.languages.map(languages => `<li>${languages}</li>`).join('')
+    }
+
+    const portfolio = document.getElementById('profile.portfolio');  // Certifique-se de que este ID exista no HTML
+if (portfolio) {
+    portfolio.innerHTML = profileData.portfolio.map(project => {
+        return `
+        <li>
+            <h3 ${project.github ? 'class="github"' : ''}>${project.name}</h3>
+            <a href="${project.url}" target="_blank">${project.url}</a>
+        </li>
+        `;
+    }).join('');  // Fechando a função map corretamente e convertendo o array de strings em uma única string
+    }
+
+    const professionalExperience = document.getElementById('profile.professionalExperience')
+    professionalExperience.innerHTML = profileData.professionalExperience.map(experience => {
+        return`
+        <li>
+        <h3 class="title">${experience.name}</h3>
+        <p class="period">${experience.period}</p>
+        <p>${experience.description}</p>
+        </li>
+        `
+    }).join('')
+        
+    }
+
 
 (async () => {
     const profileData = await fetchProfileData()
     updateProfileInfo(profileData)
-})()
+})();
