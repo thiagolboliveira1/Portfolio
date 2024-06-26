b73a419ff7667e1400ced56f706adbec4087536b
thiagolboliveira1 <thiagolboliveira@hotmail.com>
2024-06-26 10:32:15 -0300

    Corrigindo erros

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,7 +1,14 @@
-
-
 async function fetchProfileData() {
     const url = 'https://raw.githubusercontent.com/thiagolboliveira1/Portfolio/main/data/profile.json'
-   const fetching = await fetch(url)
-   return await fetching.json()
+    try {
+        const fetching = await fetch(url)
+        if (!fetching.ok) {
+            throw new Error(`Network response was not ok: ${fetching.statusText}`)
+        }
+        return await fetching.json()
+    } catch (error) {
+        console.error('Failed to fetch profile data:', error)
+        throw error // Re-throw the error after logging it
+    }
 }
+
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,37 @@
+function updateProfileInfo(profileData) {
+    const photo = document.getElementById('profile.photo')
+    if (photo) {
+        photo.src = profileData.photo
+        photo.alt = profileData.name
+    }
+
+    const name = document.getElementById('profile.name')
+    if (name) name.innerText = profileData.name
+
+    const job = document.getElementById('profile.job')
+    if (job) job.innerText = profileData.job
+
+    const location = document.getElementById('profile.location')
+    if (location) location.innerText = profileData.location
+
+    const phone = document.getElementById('profile.phone')
+    if (phone) {
+        phone.innerText = profileData.phone
+        if (phone.tagName === 'A') {  // Verifica se o elemento é um link
+            phone.href = `tel:${profileData.phone}`
+        }
+    }
+
+    const email = document.getElementById('profile.email')
+    if (email) {
+        email.innerText = profileData.email
+        if (email.tagName === 'A') {  // Verifica se o elemento é um link
+            email.href = `mailto:${profileData.email}`
+        }
+    }
+}
 
 (async () => {
-
-const profileData = await fetchProfileData()
-console.log(profileData)
-
+    const profileData = await fetchProfileData()
+    updateProfileInfo(profileData)
 })()
